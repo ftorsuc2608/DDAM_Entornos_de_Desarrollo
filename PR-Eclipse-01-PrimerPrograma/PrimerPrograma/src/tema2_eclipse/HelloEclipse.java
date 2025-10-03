4486a95a56818c9eafb83250244b161f96abafd0
ftorsuc2608 <ftorsuc2608@g.educaand.es>
2025-10-03 14:41:22 +0200

    Add files via upload

---
+++ PR-Eclipse-01-PrimerPrograma/PrimerPrograma/src/tema2_eclipse/HelloEclipse.java
@@ -0,0 +1,28 @@
+package tema2_eclipse;
+/*
+ *	Alumno: Federico Miguel Torrecillas Such
+ *	Fecha: 03/10/2025
+ */
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+		//Imprime mensajes en consola 
+		System.out.println("¡Mi primer programa!");
+		System.out.println("Reglas básicas de Java:");
+		System.out.println("1) Distingue MAYÚSCULAS y minúsculas");
+		System.out.println("2) Cada instrucción termina con ;");
+		
+		//Define y muestra una variable
+		int contadorMensajes = 0;
+		contadorMensajes++;
+		System.out.println("Mensajes impresos: " + contadorMensajes);
+		
+		//Correción de formato
+		int x = 3;
+		System.out.print("Este código no está");
+		System.out.print(" muy bien ");
+		System.out.print("tabulado");
+		x = 7;
+	}
+
+}
